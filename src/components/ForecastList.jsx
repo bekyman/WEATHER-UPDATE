03a9661e8d2ef10f694e904b80b8949b92b735bb
bekyman <51075542+bekyman@users.noreply.github.com>
2026-02-16 17:48:11 +0300

    Update ForecastList.jsx

--- src/components/ForecastList.jsx
+++ src/components/ForecastList.jsx
@@ -1,1 +1,18 @@
-
+export default function ForecastList({ forecast }) {
+  return (
+    <div className="flex gap-4 overflow-x-auto mt-6">
+      {forecast.map((item, i) => (
+        <div key={i} className="bg-white p-4 rounded-xl shadow min-w-[120px] text-center">
+          <p className="font-semibold">
+            {new Date(item.dt_txt).toLocaleDateString()}
+          </p>
+          <img
+            src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+            className="mx-auto"
+          />
+          <p>{Math.round(item.main.temp)}°C</p>
+        </div>
+      ))}
+    </div>
+  );
+}
