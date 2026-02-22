b9b5696a9cb54d3a30f303f75d9469b9f153429f
bekyman <51075542+bekyman@users.noreply.github.com>
2026-02-22 19:00:52 +0300

    Implement geolocation for weather fetching
    Added geolocation functionality to fetch weather data based on user's location.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,28 @@
+import React, { useEffect } from 'react'; 
 import SearchBar from '../components/SearchBar';
 import WeatherCard from '../features/weather/components/WeatherCard';
 import ForecastList from '../features/weather/components/ForecastList';
-import UnitToggle from '../features/weather/components/UnitToggle';
+import UnitToggle from '../features/weather/components/UnitToggle'; 
+
+
+import { useGeolocation } from '../hooks/useGeolocation'; 
+import { useWeather } from '../features/weather/hooks/useWeather';
 
 function App() {
+  
+  const { getPosition, coords } = useGeolocation();
+  const { fetchWeatherByCoords } = useWeather(); 
+
+  useEffect(() => {
+    getPosition(); 
+  }, [getPosition]); 
+
+  useEffect(() => {
+    if (coords) {
+      fetchWeatherByCoords(coords.lat, coords.lon);
+    }
+  }, [coords, fetchWeatherByCoords]);
+
   return (
     <div className="app-container">
       <header>
@@ -18,6 +37,6 @@ function App() {
       </main>
     </div>
   );
-}
+} 
 
 export default App;
