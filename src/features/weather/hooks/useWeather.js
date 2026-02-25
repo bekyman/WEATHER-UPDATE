f5159277bc211d8bff9c0c052eeb0578dd3f6c3c
bekyman <51075542+bekyman@users.noreply.github.com>
2026-02-25 23:56:26 +0300

    Update useWeather.js

--- src/features/weather/hooks/useWeather.js
+++ src/features/weather/hooks/useWeather.js
@@ -1,27 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 import { getWeatherByCoords } from "../api/weatherApi";
 
 export const useWeather = () => {
-  const [weather, setWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const getWeather = async (city) => {
+  
+  const fetchWeather = async (lat, lon) => {
     setLoading(true);
     setError(null);
     try {
-      const weatherData = await fetchWeatherData(city);
-      setWeather(weatherData);
-      
-      const forecastData = await fetchForecastData(weatherData.coord.lat, weatherData.coord.lon);
-      setForecast(forecastData);
+      const data = await getWeatherByCoords(lat, lon);
+      setWeatherData(data);
     } catch (err) {
-      setError(err.message);
+      console.error(err);
+      setError(err.message || "Failed to fetch weather");
     } finally {
       setLoading(false);
     }
   };
 
-  return { weather, forecast, loading, error, getWeather };
+ 
+  useEffect(() => {
+    
+    const defaultLat = 9.03; 
+    const defaultLon = 38.74; 
+    fetchWeather(defaultLat, defaultLon);
+  }, []);
+
+  return { weatherData, loading, error, fetchWeather };
 };
