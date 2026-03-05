3148c53918c3f25d01f85d015a3d11469b28d39b
bekyman <51075542+bekyman@users.noreply.github.com>
2026-03-05 15:44:17 +0300

    Delete src/features/weather/api directory

--- src/features/weather/api/weatherApi.js
+++
@@ -1,8 +0,0 @@
-export const getWeatherByCoords = async (lat, lon, unit = 'metric') => {
-  const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=${API_KEY}`;
-  
-  const response = await fetch(url);
-  if (!response.ok) throw new Error("Failed to get weather for your location.");
-  return await response.json();
-};
