9ae36ae70ca686daa0ef8499c1a2b52dd99f4f46
bekyman <51075542+bekyman@users.noreply.github.com>
2026-03-05 16:39:48 +0300

    Create getUserLocation.js

---
+++ src/utils/getUserLocation.js
@@ -0,0 +1,17 @@
+export function getUserLocation() {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      reject("Geolocation not supported");
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        resolve({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude
+        });
+      },
+      () => reject("Location permission denied")
+    );
+  });
+}
